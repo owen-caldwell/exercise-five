25c1cf2bb61a53d27e2a67b7f4e62c60ce02cff8
Owen Caldwell <caldwellom@gmail.com>
2024-11-05 08:21:25 -0500

    first commit

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express()
 const port = 3000;
 
 const indexRoute = require("./routes/index.js");
-const aboutRoute = require("./routes/about.js");
+const createPostRoute = require("./routes/createPost.js");
+const singlePostRoute = require("./routes/singlePost.js");
 
 app.use("/", indexRoute);
-app.use("/about", aboutRoute);
-
-app.use(express.static("public"))
+app.use("/createPost", createPostRoute);
+app.use("/post", singlePostRoute);
+// app.use(express.static("public"))
 
 app.listen(port, () => {
     console.log(`Cool app listening on port ${port}`)
 })
 
-module.exports = app
+// module.exports = app
